41333028f249557549dcbcf1b30c46525124284a
Lana Tinawi <ltinawi@luc.edu>
2023-12-05 14:08:50 -0600

    minor edits

---
+++ Screens/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function HomePage() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home page placeholder</Text>
+      </View>
+    );
+  }
+  export default HomePage; 
