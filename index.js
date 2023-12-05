6a1406fd3109997c5d95a5067d956c9dc467e9dc
Lana Tinawi <ltinawi@luc.edu>
2023-12-05 14:05:46 -0600

    minor edits

--- index.js
+++ index.js
@@ -8,11 +8,4 @@ import {name as appName} from './app.json';
 import MobileDev from '../MobileDev';
 
 AppRegistry.registerComponent('MobileDev', () => MobileDev);
-export const theme = {
-    background: '#eab308',
-    text: '#eab308'
- }
- export const styles = {
-    text: {color: theme.text},
-    background: {backgroundColor: theme.background}
- }
+
